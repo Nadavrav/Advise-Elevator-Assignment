d83765603ac0e1f5cd76f26de4e1a0c3275607bb
Nadavrav <nadavrav@post.bgu.ac.il>
2025-07-21 12:23:55 +0300

    added db settings

---
+++ backend/Elevator.Api/Migrations/20250721092028_InitialCreate.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elevator.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberOfFloors = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Buildings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ElevatorCalls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestedFloor = table.Column<int>(type: "int", nullable: false),
+                    DestinationFloor = table.Column<int>(type: "int", nullable: true),
+                    CallTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsHandled = table.Column<bool>(type: "bit", nullable: false),
+                    BuildingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ElevatorCalls", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ElevatorCalls_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Elevators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrentFloor = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Direction = table.Column<int>(type: "int", nullable: false),
+                    DoorStatus = table.Column<int>(type: "int", nullable: false),
+                    BuildingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elevators", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Elevators_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ElevatorCallAssignments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AssignmentTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ElevatorId = table.Column<int>(type: "int", nullable: false),
+                    ElevatorCallId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ElevatorCallAssignments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ElevatorCallAssignments_ElevatorCalls_ElevatorCallId",
+                        column: x => x.ElevatorCallId,
+                        principalTable: "ElevatorCalls",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ElevatorCallAssignments_Elevators_ElevatorId",
+                        column: x => x.ElevatorId,
+                        principalTable: "Elevators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_UserId",
+                table: "Buildings",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ElevatorCallAssignments_ElevatorCallId",
+                table: "ElevatorCallAssignments",
+                column: "ElevatorCallId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ElevatorCallAssignments_ElevatorId",
+                table: "ElevatorCallAssignments",
+                column: "ElevatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ElevatorCalls_BuildingId",
+                table: "ElevatorCalls",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elevators_BuildingId",
+                table: "Elevators",
+                column: "BuildingId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ElevatorCallAssignments");
+
+            migrationBuilder.DropTable(
+                name: "ElevatorCalls");
+
+            migrationBuilder.DropTable(
+                name: "Elevators");
+
+            migrationBuilder.DropTable(
+                name: "Buildings");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
